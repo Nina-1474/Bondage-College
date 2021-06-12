5cb052d6ac9e60c4b9b8972fa0e010c6d3c2928c
Nina-1474 <62667047+Nina-1474@users.noreply.github.com>
2021-06-13 00:02:38 +0100

    Locks variable type revert

--- BondageClub/Screens/Character/Login/Login.js
+++ BondageClub/Screens/Character/Login/Login.js
@@ -358,7 +358,7 @@ function LoginDifficulty() {
 function LoginExtremeItemSettings() {
 	Player.BlockItems = [];
 	// If the permissions are "Owner/Lover/Whitelist" don't limit the locks so that whitelist can use them
-	Player.LimitedItems = (Player.ItemPermission == 3) ? [] : MainHallStrongLocks;
+	Player.LimitedItems = (Player.ItemPermission == 3) ? [] : MainHallStrongLocks.map(L => { return { Name: L, Group: "ItemMisc", Type: null }; };
 	Player.HiddenItems = [];
 }
 
